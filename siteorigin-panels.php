bbb146fe6fcfb606feefe1584d69177eeae7bfea
Sean Kim <seanwoojinkim@gmail.com>
2014-12-30 23:04:08 -0800

    Update siteorigin-panels.php
    Argh, my mistake. I had written this filter for an older version of panels, and realized when I updated to the latest that it was referencing a $panels_data which was no longer in scope in this version. I simply removed the reference to the $panels_data and now it works fine.
    
    Apologies.

--- siteorigin-panels.php
+++ siteorigin-panels.php
@@ -981,7 +981,7 @@ function siteorigin_panels_the_widget( $widget, $instance, $grid, $cell, $panel,
 
 	if( empty($post_id) ) $post_id = get_the_ID();
 
-	$classes = apply_filters( 'siteorigin_panels_widget_classes', array( 'panel', 'widget' ), $panels_data );
+	$classes = apply_filters( 'siteorigin_panels_widget_classes', array( 'panel', 'widget' ));
 	if ( !empty( $the_widget ) && !empty( $the_widget->id_base ) ) $classes[] = 'widget_' . $the_widget->id_base;
 	if ( $is_first ) $classes[] = 'panel-first-child';
 	if ( $is_last ) $classes[] = 'panel-last-child';
