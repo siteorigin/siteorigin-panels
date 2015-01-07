6fd704f9bd19fd35ca0415fe5856ccb742dd5fc1
gregpriday <greg@siteorigin.com>
2015-01-07 16:19:18 +0200

    Generate panels_data in JS
    panels_data was being json encoded in PHP and put into a hidden input field. This sometimes caused issues because of how json_encode and esc_attr work.
    
    This change lets Javascript handle this instead.

--- tpl/metabox-panels.php
+++ tpl/metabox-panels.php
@@ -1,8 +1,15 @@
+<?php
+$builder_id = uniqid();
+?>
+
 <div id="siteorigin-panels-metabox" class="siteorigin-panels-builder">
 	<?php do_action('siteorigin_panels_before_interface') ?>
 	<?php wp_nonce_field('save', '_sopanels_nonce') ?>
 
-	<input name="panels_data" value="<?php echo esc_attr( json_encode( $panels_data ) ) ?>" type="hidden" class="siteorigin-panels-data-field" />
+	<input name="panels_data" value="" type="hidden" class="siteorigin-panels-data-field" id="panels-data-field-<?php echo esc_attr($builder_id) ?>" />
+	<script type="text/javascript">
+		document.getElementById('panels-data-field-<?php echo esc_attr($builder_id) ?>').value = JSON.stringify( JSON.parse("<?php echo addslashes(json_encode($panels_data)) ?>") );
+	</script>
 
 	<?php do_action('siteorigin_panels_metabox_end'); ?>
 </div>
