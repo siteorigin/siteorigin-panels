45a257fe40750cf0a85b9403a40b53b488823e9e
Adrian Dimitrov <dimitrov.adrian@gmail.com>
2015-02-24 23:00:08 +0200

    Update siteorigin-panels.php

--- siteorigin-panels.php
+++ siteorigin-panels.php
@@ -771,17 +771,19 @@ function siteorigin_panels_render( $post_id = false, $enqueue_css = true, $panel
 		}
 	}
 
-	$last_gi = 0;
-	$last_ci = 0;
-	$last_wi = 0;
-	foreach ($panels_data['widgets'] as $wid => &$widget_info) {
-		if ($widget_info['panels_info']['grid'] != $last_gi) {
-			$last_wi = 0;
+	if (!empty($panels_data['widgets'])) {
+		$last_gi = 0;
+		$last_ci = 0;
+		$last_wi = 0;
+		foreach ($panels_data['widgets'] as $wid => &$widget_info) {
+			if ($widget_info['panels_info']['grid'] != $last_gi) {
+				$last_wi = 0;
+			}
+			elseif ($widget_info['panels_info']['cell'] != $last_ci) {
+				$last_wi = 0;
+			}
+			$widget_info['panels_info']['panel'] = $last_wi++;
 		}
-		elseif ($widget_info['panels_info']['cell'] != $last_ci) {
-			$last_wi = 0;
-		}
-		$widget_info['panels_info']['panel'] = $last_wi++;
 	}
 
 	$panels_data = apply_filters( 'siteorigin_panels_data', $panels_data, $post_id );
