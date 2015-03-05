08ddb26b18c1a197c5d8e8e40add0628c72757c9
gregpriday <greg@siteorigin.com>
2015-03-05 07:03:14 +0200

    Performance enhancements and style changes

--- widgets/siteorigin-panels-widgets.php
+++ widgets/siteorigin-panels-widgets.php
@@ -1,19 +1,39 @@
 <?php
 
 //TODO: Ideally we would be handling widgets the same as WordPress does, then none of this would be necessary, but until then, this works.
-class SiteOrigin_Panels_Widgets {
+class SiteOrigin_Panels_Widget_Sidebars {
 
 	private $all_posts_widgets;
 
 	function __construct() {
 		$this->all_posts_widgets = array();
 
-		add_action( 'widgets_init', array( $this, 'register_widgets' ) , 20 );
+		add_filter( 'wp', array( $this, 'register_widgets' ) , 20 );
 		add_filter( 'sidebars_widgets', array( $this, 'add_widgets_to_sidebars' ) );
 	}
 
+	/**
+	 * Get the single instance.
+	 *
+	 * @return SiteOrigin_Panels_Widgets
+	 */
+	static function single(){
+		static $single = false;
+		if( empty($single) ) $single = new SiteOrigin_Panels_Widget_Sidebars();
+
+		return $single;
+	}
+
+	/**
+	 * @param string $name The name of the function
+	 * @param array $args
+	 *
+	 * @return mixed
+	 */
 	function __call( $name, $args ) {
-		preg_match( '/call_widget_(.+)/', $name, $opt_matches );
+
+		// Check if this is a filter option call
+		preg_match( '/filter_option_widget_(.+)/', $name, $opt_matches );
 		if ( !empty( $opt_matches ) && count( $opt_matches ) > 1 ) {
 			$opt_name = $opt_matches[1];
 			global $wp_widget_factory;
@@ -35,62 +55,63 @@ class SiteOrigin_Panels_Widgets {
 
 			return $args[0];
 		}
+
 	}
 
-	function register_widgets() {
-		$pb_posts = new WP_Query( array(
-			'meta_key' => 'panels_data',
-			'post_type' => array( 'post', 'page' ),
-			'post_status' => 'publish',
-			'nopaging' => true,
-		) );
+	/**
+	 * Register all the current widgets so we can filter the get_option('widget_...') values to add instances
+	 */
+	function register_widgets( ) {
+		global $wp_query;
+		if( !$wp_query->is_singular() ) return;
+
+		$pb_post = $wp_query->post;
+
 		global $wp_widget_factory;
 		$widget_option_names = array();
-		foreach ( $pb_posts->posts as $pb_post ) {
-			$panels_data = get_post_meta( $pb_post->ID, 'panels_data', true );
-			if( empty( $panels_data ) || empty( $panels_data['widgets'] ) ) {
-				continue;
-			}
-			$widgets = $panels_data['widgets'];
-			$this->all_posts_widgets[ $pb_post->ID ] = array();
-			foreach ( $widgets as $widget_instance ) {
-				$id_val = $pb_post->ID . strval( 1000 + intval( $widget_instance['panels_info']['id'] ) );
-				$widget_class = $widget_instance['panels_info']['class'];
-				if ( ! empty( $wp_widget_factory->widgets[ $widget_class ] ) ) {
-					$widget = $wp_widget_factory->widgets[ $widget_class ];
-					$widget_instance['id'] = $widget->id_base . '-' . $id_val;
-					$widget_option_names[] = $widget->option_name;
-				}
-				$this->all_posts_widgets[$pb_post->ID][] = $widget_instance;
-			}
+		$panels_data = get_post_meta( $pb_post->ID, 'panels_data', true );
+		if( empty( $panels_data ) || empty( $panels_data['widgets'] ) ) {
+			return;
 		}
+		$widgets = $panels_data['widgets'];
+		$this->all_posts_widgets[ $pb_post->ID ] = array();
+		foreach ( $widgets as $widget_instance ) {
+			$id_val = $pb_post->ID . strval( 1000 + intval( $widget_instance['panels_info']['id'] ) );
+			$widget_class = $widget_instance['panels_info']['class'];
+			if ( ! empty( $wp_widget_factory->widgets[ $widget_class ] ) ) {
+				$widget = $wp_widget_factory->widgets[ $widget_class ];
+				$widget_instance['id'] = $widget->id_base . '-' . $id_val;
+				$widget_option_names[] = $widget->option_name;
+			}
+			$this->all_posts_widgets[$pb_post->ID][] = $widget_instance;
+		}
+
 		$widget_option_names = array_unique( $widget_option_names );
 		foreach ( $widget_option_names as $widget_option_name ) {
-			add_filter( 'option_' . $widget_option_name, array( $this, 'call_' . $widget_option_name ) );
+			add_filter( 'option_' . $widget_option_name, array( $this, 'filter_option_' . $widget_option_name ) );
 		}
 	}
 
 	/**
 	 * Add a sidebar for SiteOrigin Panels widgets so they are correctly detected by is_active_widget
+	 *
+	 * @param $sidebars_widgets
+	 * @return array
 	 */
 	function add_widgets_to_sidebars( $sidebars_widgets ) {
-		global $wp_query;
-		if ( ! empty( $this->all_posts_widgets ) ) {
-			$siteorigin_panels_widget_ids = array();
-			foreach ( $wp_query->posts as $post ) {
-				if ( ! empty( $this->all_posts_widgets[ $post->ID ] ) ) {
-					$post_widgets = $this->all_posts_widgets[ $post->ID ];
-					foreach ( $post_widgets as $widget_instance ) {
-						//Sidebars widgets and the global $wp_registered widgets use full widget ids as keys
-						$siteorigin_panels_widget_ids[] = $widget_instance['id'];
-					}
-					if( ! empty( $siteorigin_panels_widget_ids) ) $sidebars_widgets['sidebar-siteorigin_panels-post-' . $post->ID] = $siteorigin_panels_widget_ids;
-				}
+		if ( empty( $this->all_posts_widgets ) ) return $sidebars_widgets;
+
+		foreach ( array_keys( $this->all_posts_widgets ) as $post_id ) {
+			$post_widgets = $this->all_posts_widgets[ $post_id ];
+			foreach ( $post_widgets as $widget_instance ) {
+				//Sidebars widgets and the global $wp_registered widgets use full widget ids as keys
+				$siteorigin_panels_widget_ids[] = $widget_instance['id'];
 			}
+			if( ! empty( $siteorigin_panels_widget_ids) ) $sidebars_widgets['sidebar-siteorigin_panels-post-' . $post_id] = $siteorigin_panels_widget_ids;
 		}
 
 		return $sidebars_widgets;
 	}
 }
-global $siteorigin_panels_widgets;
-$siteorigin_panels_widgets = new SiteOrigin_Panels_Widgets();
+
+SiteOrigin_Panels_Widget_Sidebars::single();
