aa8e8eac0182e03832c5f22edbf50a4c223cf641
gregpriday <greg@siteorigin.com>
2015-03-21 22:34:44 +0200

    Moved panels_data filtering
    Moved panels_data filtering to earlier in siteorigin_panels_render.

--- siteorigin-panels.php
+++ siteorigin-panels.php
@@ -793,26 +793,28 @@ function siteorigin_panels_render( $post_id = false, $enqueue_css = true, $panel
 		}
 	}
 
-	if (!empty($panels_data['widgets'])) {
+	$panels_data = apply_filters( 'siteorigin_panels_data', $panels_data, $post_id );
+	if( empty( $panels_data ) || empty( $panels_data['grids'] ) ) return '';
+
+	// Filter the widgets to add indexes
+	if ( !empty( $panels_data['widgets'] ) ) {
 		$last_gi = 0;
 		$last_ci = 0;
 		$last_wi = 0;
-		foreach ($panels_data['widgets'] as $wid => &$widget_info) {
-			if ($widget_info['panels_info']['grid'] != $last_gi) {
+		foreach ( $panels_data['widgets'] as $wid => &$widget_info ) {
+
+			if ( $widget_info['panels_info']['grid'] != $last_gi ) {
 				$last_gi = $widget_info['panels_info']['grid'];
 				$last_ci = 0;
 				$last_wi = 0;
 			}
-			elseif ($widget_info['panels_info']['cell'] != $last_ci) {
+			elseif ( $widget_info['panels_info']['cell'] != $last_ci ) {
 				$last_ci = $widget_info['panels_info']['cell'];
 				$last_wi = 0;
 			}
 			$widget_info['panels_info']['cell_index'] = $last_wi++;
 		}
 	}
-
-	$panels_data = apply_filters( 'siteorigin_panels_data', $panels_data, $post_id );
-	if( empty( $panels_data ) || empty( $panels_data['grids'] ) ) return '';
 
 	if( is_rtl() ) $panels_data = siteorigin_panels_make_rtl( $panels_data );
 
