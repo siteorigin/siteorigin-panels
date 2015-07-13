d69d75fa9f26c54e11aa9caf982b6e7485897e99
Braam Genis <braamgenis@gmail.com>
2015-07-13 15:33:22 +0200

    Selenium test for moving a widget. Config file for base test server url.

--- selenium-tests/test-page-builder.js
+++ selenium-tests/test-page-builder.js
@@ -1,4 +1,5 @@
-var webdriver = require('selenium-webdriver'),
+var baseUrl = require('./config.js').baseUrl,
+    webdriver = require('selenium-webdriver'),
     Key = webdriver.Key,
     By = webdriver.By,
     until = webdriver.until,
@@ -15,7 +16,7 @@ test.describe('Page Builder', function() {
 
     test.before(function () {
         driver = new chrome.Driver();
-        driver.get('http://local.wordpress.dev/wp-login.php');
+        driver.get(baseUrl + 'wp-login.php');
         driver.findElement(By.id('user_login')).sendKeys('admin');
         driver.findElement(By.id('user_pass')).sendKeys('password');
         driver.findElement(By.id('wp-submit')).click();
@@ -26,17 +27,17 @@ test.describe('Page Builder', function() {
         driver.quit();
     });
 
-    test.xit('should create the Page Builder tab.', function () {
-        driver.get('http://local.wordpress.dev/wp-admin/post-new.php?post_type=page');
-        driver.wait(until.titleContains('Add New Page'), 3000);
-        driver.wait(until.elementLocated(By.id('content-panels')), 3000);
+    test.it('should create the Page Builder tab.', function () {
+        driver.get(baseUrl + 'wp-admin/post-new.php?post_type=page');
+        driver.wait(until.titleContains('Add New Page'), 500);
+        driver.wait(until.elementLocated(By.id('content-panels')), 500);
     });
 
-    test.xit('should successfully add a row.', function () {
-        driver.get('http://local.wordpress.dev/wp-admin/post-new.php?post_type=page');
-        driver.wait(until.elementLocated(By.id('content-panels')), 3000);
+    test.it('should successfully add a row.', function () {
+        driver.get(baseUrl + 'wp-admin/post-new.php?post_type=page');
+        driver.wait(until.elementLocated(By.id('content-panels')), 500);
         driver.findElement(By.id('content-panels')).click();
-        driver.wait(until.elementLocated(By.className('so-row-add')), 3000);
+        driver.wait(until.elementLocated(By.className('so-row-add')), 500);
         driver.findElement(By.className('so-row-add')).click();
         driver.wait(until.elementLocated(By.className('so-panels-dialog-wrapper')), 500);
         driver.wait(until.elementLocated(By.className('so-insert')));
@@ -44,8 +45,8 @@ test.describe('Page Builder', function() {
         driver.wait(until.elementLocated(By.className('so-row-container')), 500);
     });
 
-    test.xit('should successfully edit a row.', function () {
-        driver.get('http://local.wordpress.dev/wp-admin/post.php?post=410&action=edit');
+    test.it('should successfully edit a row.', function () {
+        driver.get(baseUrl + 'wp-admin/post.php?post=410&action=edit');
         driver.wait(until.elementLocated(By.css('span.so-dropdown-wrapper > a.so-row-settings')), 500);
         driver.findElement(By.css('span.so-dropdown-wrapper > a.so-row-settings')).click();
         driver.wait(until.elementLocated(By.css('div.so-content.panel-dialog > div.row-set-form > input[name="cells"]')), 500);
@@ -58,13 +59,21 @@ test.describe('Page Builder', function() {
     });
 
     test.it('should successfully add a widget.', function () {
-        driver.get('http://local.wordpress.dev/wp-admin/post-new.php?post_type=page');
-        driver.wait(until.elementLocated(By.id('content-panels')), 3000);
+        driver.get(baseUrl + 'wp-admin/post-new.php?post_type=page');
+        driver.wait(until.elementLocated(By.id('content-panels')), 500);
         driver.findElement(By.id('content-panels')).click();
         driver.wait(until.elementLocated(By.className('so-widget-add')), 500);
         driver.findElement(By.className('so-widget-add')).click();
         driver.wait(until.elementLocated(By.className('so-panels-dialog-wrapper')), 500);
         driver.findElement(By.js('return jQuery(\'li.widget-type h3:contains("Text")\')')).click(); //0_o
         driver.wait(until.elementLocated(By.css('.so-row-container > .so-cells > .cell > .cell-wrapper > .so-widget')), 500);
+    });
+
+    test.it('should successfully move a widget.', function () {
+        driver.get(baseUrl + 'wp-admin/post.php?post=410&action=edit');
+        var ofWidget = '.so-row-container > .so-cells > .cell > .cell-wrapper > .so-widget';
+        driver.wait(until.elementLocated(By.css(ofWidget)), 500);
+        var emptyCell = driver.findElement(By.js('return jQuery(\'.so-row-container > .so-cells > .cell > .cell-wrapper\')[1];')); //0_o
+        driver.actions().dragAndDrop(driver.findElement(By.css(ofWidget)), emptyCell).perform();
     });
 });
