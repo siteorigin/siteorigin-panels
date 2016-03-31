8199fa5013f372f306b529eb687fa966b88690c4
gregpriday <greg@siteorigin.com>
2016-03-31 09:02:20 +0200

    Enhancements to scrolling in live editor preview

--- js/live-editor/live-editor-front.js
+++ js/live-editor/live-editor-front.js
@@ -25,18 +25,10 @@ function liveEditorScrollTo( el ){
 		$el = $( el ),
 		rect = $el[0].getBoundingClientRect();
 
-	if( rect.top >= 0 &&
-	    rect.left >= 0 &&
-	    rect.bottom <= $(window).height() &&
-	    rect.right <= $(window).width()
-	) {
-		// This is already in the viewport, don't need to do anything
-		return;
-	} else {
-
+	if( rect.top <= 0 || rect.bottom >= $(window).height() ) {
 		var newScrollTop = 0;
 
-		if( rect.top < 0 ) {
+		if( rect.top < 0 || $el.height() >= $( window ).height() * 0.8 ) {
 			// Scroll up to the element
 			newScrollTop = $( window ).scrollTop() + rect.top - 150;
 		} else if( rect.bottom > $(window).height() ) {
