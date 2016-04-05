4e6e6876173af21445285640cdb973af687b57be
gregpriday <greg@siteorigin.com>
2016-04-05 21:50:48 +0200

    Process raw widgets in the live editor preview

--- inc/live-editor.php
+++ inc/live-editor.php
@@ -12,6 +12,11 @@
 function siteorigin_panels_live_editor($value, $post_id, $meta_key){
 	if( $meta_key == 'panels_data' && current_user_can( 'edit_post', $post_id ) && !empty( $_POST['live_editor_panels_data'] ) ) {
 		$data = json_decode( wp_unslash( $_POST['live_editor_panels_data'] ), true );
+
+		if( !empty( $data['widgets'] ) ) {
+			$data['widgets'] = siteorigin_panels_process_raw_widgets( $data['widgets'] );
+		}
+
 		$value = array( $data );
 	}
 
