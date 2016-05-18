76abd45b0b29037e1029a5ce7990afee963732fc
gregpriday <greg@siteorigin.com>
2016-05-18 11:09:43 +0200

    Change how window height is calculated
    If the wrapper is taller than the window, then we treat this wrapper height as the window height. This makes the parallax work while scrolling through the entire wrapper.

--- js/siteorigin-parallax.js
+++ js/siteorigin-parallax.js
@@ -67,37 +67,41 @@
 				var percent = ( position - 1 ) / - 2;
 				var topPosition = 0;
 				var limitScale = 1;
+				var backgroundSize = options.backgroundSize;
+
+				var effectiveWindowHeight = $( window ).outerHeight();
+				if( wrapperSize[1] > effectiveWindowHeight ) {
+					effectiveWindowHeight = wrapperSize[1];
+				}
 
 				// Do the setup for every time something changes
 				if( options.backgroundSizing === 'scaled' ) {
 					// The amount of X scaling
-					var scaleX = wrapperSize[0] / options.backgroundSize[0];
+					var finalY = wrapperSize[1] / limitMotion; // This is the required Y height to create a parallax effect
+					var scaleBG = wrapperSize[0] / backgroundSize[0]; // The initial scaling is based on container width
 
-					var finalY = wrapperSize[1] / limitMotion;
-					var scaleBG = scaleX;
-
-					if( finalY > options.backgroundSize[1] || options.backgroundSize[1] * scaleX < finalY ) {
+					if( finalY > backgroundSize[1] * scaleBG ) {
 						// This image wouldn't be tall enough to give a decent parallax effect, so we'll scale it up
-						scaleBG = finalY / options.backgroundSize[1];
+						scaleBG = finalY / backgroundSize[1];
 					}
 
 					$$.css(
 						'background-size',
-						( options.backgroundSize[0] * scaleBG ) + 'px ' +
-						( options.backgroundSize[1] * scaleBG ) + 'px'
+						( backgroundSize[0] * scaleBG ) + 'px ' +
+						( backgroundSize[1] * scaleBG ) + 'px'
 					);
 
 					// Work out the top position
-					if( bounding.top > - wrapperSize[1] && bounding.bottom - $( window ).outerHeight() < wrapperSize[1] ) {
+					if( bounding.top > - wrapperSize[1] && bounding.bottom - effectiveWindowHeight < wrapperSize[1] ) {
 						// This is the scaled background height
-						var backgroundHeight = options.backgroundSize[1] * scaleBG;
+						var backgroundHeight = backgroundSize[1] * scaleBG;
 
 						// Check if we need to limit the amount of motion in the background image
-						if( limitMotion && backgroundHeight > $( window ).outerHeight() * limitMotion ) {
+						if( limitMotion && backgroundHeight > effectiveWindowHeight * limitMotion ) {
 							// Work out how much to scale percent position based on how much motion we want.
-							limitScale = ( $( window ).outerHeight() * limitMotion ) / ( backgroundHeight );
+							limitScale = ( effectiveWindowHeight * limitMotion ) / ( backgroundHeight );
 							// Percent is scaled so that the midpoint is still 0.5
-							percent = (percent * limitScale) + ( ( 1 - limitScale ) / 2 );
+							percent = ( percent * limitScale ) + ( ( 1 - limitScale ) / 2 );
 						}
 
 						topPosition = - ( backgroundHeight - wrapperSize[1] ) * percent;
@@ -107,13 +111,13 @@
 
 				} else if( options.backgroundSizing === 'original' ) {
 					// See scaled version or explanation of this code.
-					if( limitMotion && options.backgroundSize[1] > $( window ).outerHeight() * limitMotion ) {
-						limitScale = ( $( window ).outerHeight() * limitMotion ) / ( options.backgroundSize[1] );
+					if( limitMotion && backgroundSize[1] > effectiveWindowHeight * limitMotion ) {
+						limitScale = ( effectiveWindowHeight * limitMotion ) / ( backgroundSize[1] );
 						percent = (percent * limitScale) + ( ( 1 - limitScale ) / 2 );
 					}
 
 					// In this case, the background height is always the background size
-					topPosition = - ( options.backgroundSize[1] - wrapperSize[1] ) * percent;
+					topPosition = - ( backgroundSize[1] - wrapperSize[1] ) * percent;
 
 					// This is a version with no scaling
 					$$.css( 'background-size', 'auto' );
