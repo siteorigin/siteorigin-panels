eb3f798412668ba3e8c50ceae2445da478bac7b1
Greg Priday <greg@siteorigin.com>
2016-09-30 09:33:01 +0200

    A theoretical rewrite
    A full theoretical rewrite. This is untested.

--- tpl/js-templates.php
+++ tpl/js-templates.php
@@ -491,7 +491,7 @@ $layouts = apply_filters( 'siteorigin_panels_prebuilt_layouts', array() );
 		</div>
 
 		<div class="content">
-			<form method="post" action="<?php echo siteorigin_panels_live_editor_preview_url() ?>" target="siteorigin-panels-history-iframe-{{%= cid %}}" class="history-form">
+			<form method="post" action="<?php echo SiteOrigin_Panels::preview_url() ?>" target="siteorigin-panels-history-iframe-{{%= cid %}}" class="history-form">
 				<input type="hidden" name="live_editor_panels_data" value="">
 			</form>
 			<iframe class="siteorigin-panels-history-iframe" name="siteorigin-panels-history-iframe-{{%= cid %}}" src=""></iframe>
