bad6c6b00405d0883ef9c3659f24ad062cc8060c
gregpriday <greg@siteorigin.com>
2017-01-22 16:30:22 +0200

    Fixed icons and learn links

--- js/siteorigin-panels.js
+++ js/siteorigin-panels.js
@@ -3307,13 +3307,13 @@ module.exports = Backbone.View.extend( {
 
 		this.trigger( 'builder_rendered' );
 
-		this.$( '.so-tip-wrapper a' ).click( function( e ){
+		this.$( 'a.so-lesson-modal' ).click( function( e ){
 			e.preventDefault();
 			var $$ = $(this).blur();
 			var newwindow = window.open(
 				$$.attr('href'),
 				'signup-window',
-				'height=450,width=650,toolbar=false'
+				'height=600,width=650,toolbar=false'
 			);
 			if ( window.focus ) {
 				newwindow.focus();
