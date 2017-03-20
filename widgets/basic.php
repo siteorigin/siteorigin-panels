a42cfebf092fee6be3e40e163b486e30dac1cfb2
gregpriday <greg@siteorigin.com>
2017-03-20 22:53:35 +0200

    Additional layout widget setup check
    Resolves GH-199

--- widgets/basic.php
+++ widgets/basic.php
@@ -98,7 +98,10 @@ class SiteOrigin_Panels_Widgets_Layout extends WP_Widget {
 			<input type="hidden" value="<?php echo esc_attr( $instance['builder_id'] ) ?>" name="<?php echo $this->get_field_name('builder_id') ?>" />
 		</div>
 		<script type="text/javascript">
-			if( typeof jQuery.fn.soPanelsSetupBuilderWidget != 'undefined' && !jQuery('body').hasClass('wp-customizer') ) {
+			if(
+				typeof jQuery.fn.soPanelsSetupBuilderWidget != 'undefined' &&
+				( ! jQuery('body').hasClass('wp-customizer') || jQuery( "#siteorigin-page-builder-widget-<?php echo esc_attr( $form_id ) ?>").closest( '.panel-dialog' ).length )
+			) {
 				jQuery( "#siteorigin-page-builder-widget-<?php echo esc_attr( $form_id ) ?>").soPanelsSetupBuilderWidget();
 			}
 		</script>
