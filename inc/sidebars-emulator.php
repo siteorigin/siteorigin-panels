cb8ea7a2d7edd8ec9e023cfa5746d484f5097d87
gregpriday <greg@siteorigin.com>
2017-03-23 11:54:20 +0200

    Code cleanup for sidebars emulator

--- inc/sidebars-emulator.php
+++ inc/sidebars-emulator.php
@@ -78,34 +78,35 @@ class SiteOrigin_Panels_Sidebars_Emulator {
 			return;
 		}
 
-		$widget_option_names = array();
 		$panels_data = get_post_meta( $post_id, 'panels_data', true );
-		$this->get_all_widgets( $post_id, $panels_data, $widget_option_names, 1 );
-
+		$widget_option_names = $this->get_widget_option_names( $post_id, $panels_data, 1 );
 		$widget_option_names = array_unique( $widget_option_names );
+
 		foreach ( $widget_option_names as $widget_option_name ) {
 			add_filter( 'option_' . $widget_option_name, array( $this, 'filter_option_' . $widget_option_name ) );
 		}
 	}
 
 	/**
-	 * Recursivly get all widgets from $panels_data and store them in $this->all_posts_widgets.
+	 * Recursivly get all widget option names from $panels_data and store widget instances in $this->all_posts_widgets.
 	 *
 	 * @param int|string $post_id
 	 * @param array $panels_data
-	 * @param array $widget_option_names Starts with an empty array and fills them with option names
 	 * @param int $start This keeps track of recursive depth
+	 *
+	 * @return array A list of widget option names from the post and its Layout Builder widgets.
 	 */
-	private function get_all_widgets( $post_id, $panels_data, & $widget_option_names, $start = 1 ) {
+	private function get_widget_option_names( $post_id, $panels_data, $start = 1 ) {
 		global $wp_widget_factory;
 		if( empty( $panels_data ) || empty( $panels_data[ 'widgets' ] ) ) {
-			return;
+			return array();
 		}
 
 		if( empty( $this->all_posts_widgets[ $post_id ] ) ) {
 			$this->all_posts_widgets[ $post_id ] = array();
 		}
 
+		$widget_option_names = array();
 		$widgets = $panels_data['widgets'];
 		foreach ( $widgets as $widget_instance ) {
 			if ( empty( $widget_instance['panels_info']['class'] ) ) {
@@ -113,20 +114,21 @@ class SiteOrigin_Panels_Sidebars_Emulator {
 			}
 
 			if( $widget_instance['panels_info']['class'] === 'SiteOrigin_Panels_Widgets_Layout' ) {
-				$this->get_all_widgets( $post_id, $widget_instance[ 'panels_data' ], $widget_option_names, ++$start );
+				// Add the widget option names from the layout widget
+				$widget_option_names = array_merge( $widget_option_names, $this->get_widget_option_names( $post_id, $widget_instance[ 'panels_data' ], ++$start ) );
 			}
-			else {
-				$id_val  = $post_id . strval( ( 10000 * $start ) + intval( $widget_instance['panels_info']['id'] ) );
-				$widget_class = $widget_instance['panels_info']['class'];
-				if ( ! empty( $wp_widget_factory->widgets[ $widget_class ] ) ) {
-					$widget                = $wp_widget_factory->widgets[ $widget_class ];
-					$widget_instance['id'] = $widget->id_base . '-' . $id_val;
-					$widget_option_names[] = $widget->option_name;
-				}
-				$this->all_posts_widgets[ $post_id ][] = $widget_instance;
 
+			$id_val  = $post_id . strval( ( 10000 * $start ) + intval( $widget_instance['panels_info']['id'] ) );
+			$widget_class = $widget_instance['panels_info']['class'];
+			if ( ! empty( $wp_widget_factory->widgets[ $widget_class ] ) ) {
+				$widget                = $wp_widget_factory->widgets[ $widget_class ];
+				$widget_instance['id'] = $widget->id_base . '-' . $id_val;
+				$widget_option_names[] = $widget->option_name;
 			}
+			$this->all_posts_widgets[ $post_id ][] = $widget_instance;
 		}
+
+		return $widget_option_names;
 	}
 
 	/**
