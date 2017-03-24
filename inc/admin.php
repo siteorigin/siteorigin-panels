ac258f368b444956f453f73274c153c1e2d0b669
gregpriday <greg@siteorigin.com>
2017-03-24 06:36:44 +0200

    Setup post_content filters
    This is important when quickly switching to and from the builder interface.

--- inc/admin.php
+++ inc/admin.php
@@ -883,6 +883,7 @@ class SiteOrigin_Panels_Admin {
 		$GLOBALS[ 'SITEORIGIN_PANELS_DATABASE_RENDER' ] = true;
 
 		do_action( 'siteorigin_panels_setup_database_render', $_POST['post_id'] );
+		SiteOrigin_Panels_Post_Content::single();
 		$post_content = SiteOrigin_Panels_Renderer::single()->render( intval( $_POST['post_id'] ), false, $panels_data );
 		echo SiteOrigin_Panels_Renderer::single()->render( intval( $_POST['post_id'] ), false, $panels_data );
 
