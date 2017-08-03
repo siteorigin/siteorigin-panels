b2c9eb89ca69085f6b8ae9b5f711c1c721149d9e
Braam Genis <braam@siteorigin.com>
2017-08-03 16:39:51 +0200

    Fixed WP Text widget wrapper broken by WP4.8.1 changes.

--- js/siteorigin-panels/view/widgets/text-widget.js
+++ js/siteorigin-panels/view/widgets/text-widget.js
@@ -4,9 +4,29 @@ var textWidget = {
 	addWidget: function( idBase, widgetContainer, widgetId ) {
 		var component = wp.textWidgets;
 
-		var widgetControl = new component.TextWidgetControl({
-			el: widgetContainer
-		});
+
+		var options = {};
+		var visualField = widgetContainer.find( '.visual' );
+		// 'visual' field and syncContainer were introduced together in 4.8.1
+		if ( visualField.length > 0 ) {
+			// If 'visual' field has no value it's a legacy text widget.
+			if ( ! visualField.val() ) {
+				return null;
+			}
+
+			var fieldContainer = $( '<div></div>' );
+			var syncContainer = widgetContainer.find( '.widget-content:first' );
+			syncContainer.before( fieldContainer );
+
+			options = {
+				el: fieldContainer,
+				syncContainer: syncContainer,
+			};
+		} else {
+			options = { el: widgetContainer };
+		}
+
+		var widgetControl = new component.TextWidgetControl( options );
 
 		widgetControl.initializeEditor();
 
