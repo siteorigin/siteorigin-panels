0fb18eed104d6b61a21869b8db2d7965530673ff
Greg Priday <greg@siteorigin.com>
2019-02-11 18:14:16 +0200

    Added welcome page with lorem ipsum

---
+++ settings/tpl/welcome.php
@@ -0,0 +1,21 @@
+<h1>Welcome To SiteOrigin Page Builder</h1>
+
+<p>
+	Suspendisse volutpat sagittis metus ut imperdiet. Fusce in ipsum vitae tellus venenatis convallis id id metus. Curabitur maximus turpis ac eleifend vestibulum. Curabitur non auctor magna, ut mollis velit. Ut blandit euismod leo, tincidunt interdum erat laoreet nec. Proin consequat augue vel sapien tempor efficitur. Phasellus tempus, mauris porttitor aliquam interdum, sem sem posuere lacus, at viverra felis libero quis felis.
+</p>
+
+<p>
+	<iframe src="https://player.vimeo.com/video/314964526" width="800" height="450" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+</p>
+
+<h2>Maecenas lacus justo, tincidunt at pulvinar id, pulvinar vel augue</h2>
+
+<p>
+	Suspendisse volutpat sagittis metus ut imperdiet. Fusce in ipsum vitae tellus venenatis convallis id id metus. Curabitur maximus turpis ac eleifend vestibulum. Curabitur non auctor magna, ut mollis velit. Ut blandit euismod leo, tincidunt interdum erat laoreet nec. Proin consequat augue vel sapien tempor efficitur. Phasellus tempus, mauris porttitor aliquam interdum, sem sem posuere lacus, at viverra felis libero quis felis.
+</p>
+
+<ul>
+	<li><a href="#">Fusce in ipsum</a></li>
+	<li><a href="#">Curabitur maximus turpis</a></li>
+	<li><a href="#">Ut blandit eui</a></li>
+</ul>
