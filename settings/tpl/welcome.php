c88b570b93184e773b7e6518013c2f859b7bbe6d
Andrew Misplon <amisplon@gmail.com>
2019-02-27 11:05:37 +0200

    Added initial welcome page content

--- settings/tpl/welcome.php
+++ settings/tpl/welcome.php
@@ -1,21 +1,43 @@
 <h1>Welcome To SiteOrigin Page Builder</h1>
 
 <p>
-	Suspendisse volutpat sagittis metus ut imperdiet. Fusce in ipsum vitae tellus venenatis convallis id id metus. Curabitur maximus turpis ac eleifend vestibulum. Curabitur non auctor magna, ut mollis velit. Ut blandit euismod leo, tincidunt interdum erat laoreet nec. Proin consequat augue vel sapien tempor efficitur. Phasellus tempus, mauris porttitor aliquam interdum, sem sem posuere lacus, at viverra felis libero quis felis.
+	Page Builder by SiteOrigin makes it easy to create responsive column based content, using the widgets you know and love. Your content will accurately adapt to all mobile devices, ensuring your site is mobile-ready.
 </p>
 
 <p>
-	<iframe src="https://player.vimeo.com/video/314964526" width="800" height="450" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+	Page Builder works with standard WordPress widgets, so you’ll always find the widget you need. We've also created the <a href="https://wordpress.org/plugins/so-widgets-bundle/" target="_blank">SiteOrigin Widgets Bundle</a> to give you a wide range of additional functionality. All the widgets are built on our powerful framework, giving you advanced forms, unlimited colours and 1500+ icons.
 </p>
 
-<h2>Maecenas lacus justo, tincidunt at pulvinar id, pulvinar vel augue</h2>
+<h2>Simple Drag and Drop</h2>
 
 <p>
-	Suspendisse volutpat sagittis metus ut imperdiet. Fusce in ipsum vitae tellus venenatis convallis id id metus. Curabitur maximus turpis ac eleifend vestibulum. Curabitur non auctor magna, ut mollis velit. Ut blandit euismod leo, tincidunt interdum erat laoreet nec. Proin consequat augue vel sapien tempor efficitur. Phasellus tempus, mauris porttitor aliquam interdum, sem sem posuere lacus, at viverra felis libero quis felis.
+	Reorder rows, move widgets, resize columns and build your page, all with your mouse. Page Builder makes it easy to nudge every detail into place, no matter how often you change your mind. We've meticulously crafted each and every interaction to make sure they all feel natural. It's intuitive content editing that doesn't sacrifice on functionality.
 </p>
 
-<ul>
-	<li><a href="#">Fusce in ipsum</a></li>
-	<li><a href="#">Curabitur maximus turpis</a></li>
-	<li><a href="#">Ut blandit eui</a></li>
-</ul>
+<h2>Live Editing</h2>
+
+<p>
+	The live editor gives you an exact preview of your content, that you can edit in real-time. You can use this to pin-point the sections and widgets you want to edit, then see those edits without leaving the page.
+Create content in a fraction of the time.
+</p>
+
+<h2>History Browser</h2>
+
+<p>
+	Worry free editing is having the freedom to experiment without fearing mistakes. The History Browser lets you roll forward and back through your changes. Preview changes, then roll back to that version without skipping a beat.
+Your personal safety net.
+</p>
+
+<h2>Row and Widget Styles</h2>
+
+<p>
+	Row and widget styles give you all the control you need to make your content uniquely your own. Change attributes like paddings, background colours and column spacing. You can also enter custom CSS and CSS classes if you need even finer grained control.
+</p>
+
+<h2>Get More Features</h2>
+
+<p>
+	SiteOrigin Premium is a single plugin that adds additional settings and functionality to Page Builder, SiteOrigin widgets and SiteOrign themes. SiteOrigin Premium also includes our next level email support service. If you need expert advice and quick replies, consider SiteOrigin Premium.
+</p>
+
+<iframe src="https://player.vimeo.com/video/314964526" width="800" height="450" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
