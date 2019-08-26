15cbc79ae97aedcf0d33e6b23544bb624a765140
Greg Priday <greg@siteorigin.com>
2019-08-26 15:27:28 +0200

    Use raw margin bottom as fallback for missing mobile margin
    Resolves GH-693

--- inc/renderer.php
+++ inc/renderer.php
@@ -216,7 +216,7 @@ class SiteOrigin_Panels_Renderer {
 				foreach ( $row['cells'] as $ci => $cell ) {
 					if ( ( $collapse_order == 'left-top' && $ci != $cell_count - 1 ) || ( $collapse_order == 'right-top' && $ci !== 0 ) ) {
 						$css->add_cell_css( $post_id, $ri, $ci, '', array(
-							'margin-bottom' => ! empty( $panels_mobile_margin_bottom ) ? $panels_mobile_margin_bottom : $panels_margin_bottom
+							'margin-bottom' => ! empty( $panels_mobile_margin_bottom ) ? $panels_mobile_margin_bottom : $settings['margin-bottom'] . 'px'
 						), $panels_mobile_width );
 					}
 				}
