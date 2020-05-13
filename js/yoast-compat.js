c5e8f1b3673f03aece005ea4b33188007358bcf2
Greg Priday <greg@siteorigin.com>
2020-05-13 19:34:27 +0200

    Clean preview HTML before sending it to Yoast

--- js/yoast-compat.js
+++ js/yoast-compat.js
@@ -17,7 +17,32 @@ jQuery(function($){
 			typeof window.soPanelsBuilderView !== 'undefined' &&
 			window.soPanelsBuilderView.contentPreview
 		) {
-			data = window.soPanelsBuilderView.contentPreview;
+			var $data = $( window.soPanelsBuilderView.contentPreview );
+
+			if( $data.find('.so-panel.widget').length === 0 ) {
+				// Skip this for empty pages
+				return data;
+			}
+
+			// Remove style tags created by Widgets Bundle
+			$data.find('style').remove();
+
+			var whitelist = [
+				'p', 'a', 'img', 'caption', 'br',
+				'blockquote', 'cite',
+				'em', 'strong', 'i', 'b',
+				'q',
+				'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+				'ul', 'ol', 'li',
+				'table', 'tr', 'th', 'td'
+			].join(',');
+
+			$data.find("*").not(whitelist).each(function() {
+				var content = $(this).contents();
+				$(this).replaceWith(content);
+			});
+
+			data = $data.html();
 		}
 
 		return data;
