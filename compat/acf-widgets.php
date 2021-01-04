bf0a26fa906d9d245adfca56f38f3cda8755d243
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2021-01-05 05:12:46 +1000

    ACF: FIx Saving Issues with not SOWB Widgets

--- compat/acf-widgets.php
+++ compat/acf-widgets.php
@@ -6,6 +6,9 @@ class SiteOrigin_Panels_Compat_ACF_Widgets {
 
 		add_action( 'siteorigin_panels_before_widget_form', array( $this, 'store_acf_widget_fields_instance' ), 10, 3 );
 		add_filter( 'acf/pre_load_value', array( $this, 'load_panels_widget_field_data' ), 10, 3 );
+
+		// Widget Saving inside of Page Builder.
+		add_filter( 'widget_update_callback', array( $this, 'acf_override_instance' ), 10, 4 );
 	}
 
 	public static function single() {
@@ -72,5 +75,23 @@ class SiteOrigin_Panels_Compat_ACF_Widgets {
 				}
 			}
 		}
+	}
+
+	/**
+	 * Restores initial ACF form data to prevent saving issue in non SOWB widgets.
+	 *
+	 * @param $instance The updated widget settings.
+	 * @param $new_instance An array of new settings.
+	 * @param $old_instance An array of old settings.
+	 * @param $this The current widget instance.
+	 *
+	 */
+	public function acf_override_instance( $instance, $widget, $old_widget, $the_widget ) {
+		// Ensure widget update is from Page Builder and there's ACF data present.
+		if ( ! empty( $widget['panels_info'] ) && ! empty( $instance['acf'] ) ) {
+			$instance['acf'] = $widget['acf'];
+		}
+
+		return $instance;
 	}
 }
