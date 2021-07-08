8da100a1185a43275a9869a4d2ac8a3430ed0a03
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2021-07-08 21:14:23 +1000

    PB Plugin Links: Add Settings Link

--- inc/admin.php
+++ inc/admin.php
@@ -166,6 +166,7 @@ class SiteOrigin_Panels_Admin {
 		}
 
 		unset( $links['edit'] );
+		$links[] = '<a href="' . admin_url( 'options-general.php?page=siteorigin_panels' ) . '">' . __( 'Settings', 'siteorigin-panels' ) . '</a>';
 		$links[] = '<a href="http://siteorigin.com/threads/plugin-page-builder/">' . __( 'Support', 'siteorigin-panels' ) . '</a>';
 
 		if( SiteOrigin_Panels::display_premium_teaser() ) {
