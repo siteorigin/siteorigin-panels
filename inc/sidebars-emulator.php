1d4c0de4b5b83f529f440c2f7c0a6afda295b659
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2022-01-11 02:16:57 +1100

    Code Structure change based on feedback
    The additional check is redundant, and while there's really no major difference performance-wise it makes sense to make the change.

--- inc/sidebars-emulator.php
+++ inc/sidebars-emulator.php
@@ -50,7 +50,7 @@ class SiteOrigin_Panels_Sidebars_Emulator {
 						if ( $instance_class == $widget_class && ! empty( $sidebar_id ) ) {
 							//The option value uses only the widget id number as keys
 							preg_match( '/-([0-9]+$)/', $sidebar_id, $num_match );
-							if ( ! empty( $num_match ) && count( $num_match ) > 1  ) {
+							if ( ! empty( $num_match ) ) {
 								$args[0][ $num_match[1] ] = $widget_instance;
 							}
 						}
