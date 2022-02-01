67d36b9ee74354e85b6e0826d675c873292fa200
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2022-02-01 21:13:25 +1000

    Compat: Add "Yoast SEO: Video Compatibility

--- compat/yoast.php
+++ compat/yoast.php
@@ -62,3 +62,24 @@ function siteorigin_yoast_sitemap_images_compat( $images, $post_id ) {
 	return $images;
 }
 add_filter( 'wpseo_sitemap_urlimages', 'siteorigin_yoast_sitemap_images_compat', 10, 2 );
+
+
+if ( function_exists( 'yoast_wpseo_video_seo_init' ) ) {
+	/**
+	 * If the Yoast SEO: Video plugin is trying to index a post, and the post has a page builder layout, render it using Page Builder.
+	 *
+	 * @param $content The content to analyze.
+	 * @param $vid  Array with video info, usually empty.
+	 * @param $post Post object.
+	 *
+	 * @return string
+	 */
+	function siteorigin_yoast_video_render_page_builder( $content, $vid, $post ) {
+		if ( ! empty( $_POST['panels_data'] ) ) { 
+			$content = SiteOrigin_Panels::renderer()->render( $post->ID );
+		}
+		return $content;
+
+	}
+	add_filter( 'wpseo_video_index_content', 'siteorigin_yoast_video_render_page_builder', 10, 3 );
+}
