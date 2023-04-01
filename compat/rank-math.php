fd5a9a91e6cdec3b9c6ca0223a4c325eda6a4652
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2023-04-02 02:50:49 +1200

    Rank Math Sitemap Compat: Remove `wp_targeted_link_rel`
    This in retrospect is excessive for this usage. This also prevents a potential error.

--- compat/rank-math.php
+++ compat/rank-math.php
@@ -4,9 +4,6 @@ function siteorigin_rank_math_sitemap( $content, $post_id ) {
 	if ( ! empty( $panels_data ) ) {
 		$GLOBALS[ 'SITEORIGIN_PANELS_PREVIEW_RENDER' ] = true;
 		$content = SiteOrigin_Panels::renderer()->render( (int) $post_id, false, $panels_data );
-		if ( function_exists( 'wp_targeted_link_rel' ) && is_array( $return ) ) {
-			$content = wp_targeted_link_rel( $return );
-		}
 		unset( $GLOBALS[ 'SITEORIGIN_PANELS_PREVIEW_RENDER' ] );
 	}
 
