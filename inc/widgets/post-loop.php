e950e9ca2956dbfaf04904599ae455f69fa5354f
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2024-03-20 01:20:29 +1300

    Post Loop: Allow Formatting HTML in Widget Title

--- inc/widgets/post-loop.php
+++ inc/widgets/post-loop.php
@@ -228,7 +228,7 @@ class SiteOrigin_Panels_Widgets_PostLoop extends WP_Widget {
 		$instance['title'] = apply_filters( 'widget_title', $instance['title'], $instance, $this->id_base );
 
 		if ( ! empty( $instance['title'] ) ) {
-			echo $args['before_title'] . esc_html( $instance['title'] ) . $args['after_title'];
+			echo $args['before_title'] . wp_kses_post( $instance['title'] ) . $args['after_title'];
 		}
 
 		global $more;
