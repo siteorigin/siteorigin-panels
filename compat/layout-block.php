418c474f93ae6c3cc556c8aa356d66a89d8f8fe3
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2024-09-26 01:02:58 +1200

    Fix typo

--- compat/layout-block.php
+++ compat/layout-block.php
@@ -125,11 +125,12 @@ class SiteOrigin_Panels_Compat_Layout_Block {
 
 		$is_editing = SiteOrigin_Panels_Admin::is_block_editor();
 
+		add_filter( 'siteorigin_panels_layout_classes', $add_custom_class_name );
+
 		if ( $is_editing ) {
-			add_filter( 'siteorigin_panels_layout_classes', $add_custom_class_name );
+			SiteOrigin_Panels_Post_Content_Filters::add_filters( true );
 		}
 
-		SiteOrigin_Panels_Post_Content_Filters::add_filters( true );
 		$rendered_layout = SiteOrigin_Panels::renderer()->render( $builder_id, ! $is_editing, $panels_data );
 
 		if ( $is_editing ) {
