53c0d153f9fa2353c0f08f8911538d742b41248b
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2025-04-14 13:22:59 +1200

    Poylang: Improve Sync Handling

--- compat/polylang.php
+++ compat/polylang.php
@@ -1,12 +1,14 @@
 <?php
 /**
- * When Polylang duplicates a post, copy over panels_data if it exists.
+ * Ensure that SiteOrigin Panels data is included in Polylang's post meta copy,
+ * and sync.
  */
 function siteorigin_polylang_include_panels_data( $keys, $sync ) {
-	if ( ! $sync ) {
+	if ( $sync ) {
 		$keys[] = 'panels_data';
 	}
 
 	return $keys;
 }
 add_filter( 'pll_copy_post_metas', 'siteorigin_polylang_include_panels_data', 10, 2 );
+add_filter( 'pll_sync_post_fields', 'siteorigin_polylang_include_panels_data', 10, 2 );
