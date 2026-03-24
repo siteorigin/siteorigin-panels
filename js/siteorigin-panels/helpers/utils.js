e4671f2f540187068b3aba71cd74a52c7f0ffce1
Andrew Misplon <amisplon@gmail.com>
2026-03-24 13:25:58 +0000

    Fix inline save persistence in layout block

--- js/siteorigin-panels/helpers/utils.js
+++ js/siteorigin-panels/helpers/utils.js
@@ -33,7 +33,17 @@ module.exports = {
 	},
 
 	saveHeartbeat: function( thisDialog ) {
+		var resetButton = function() {
+			jQuery( '.so-saveinline' ).removeAttr( 'disabled' );
+		};
+
 		jQuery( '.so-saveinline' ).attr( 'disabled', 'disabled' )
+
+		if ( this.shouldUseBlockEditorSave( thisDialog ) ) {
+			this.saveBlockEditor( thisDialog, resetButton );
+			return;
+		}
+
 		jQuery( document ).one( 'heartbeat-send', function( event, data ) {
 			data.panels = JSON.stringify( {
 				data: thisDialog.builder.model.getPanelsData(),
@@ -42,39 +52,150 @@ module.exports = {
 			} );
 		} );
 		jQuery( document ).one( 'heartbeat-tick', function( event, data ) {
-			jQuery( '.so-saveinline' ).removeAttr( 'disabled' )
+			resetButton();
 		} );
 
-		if ( ! this.attemptToSave() ) {
+		if (
+			typeof wp !== 'undefined' &&
+			wp.heartbeat &&
+			wp.heartbeat.connectNow
+		) {
+			wp.heartbeat.connectNow();
+			return;
+		}
+
+		if (
+			typeof wp !== 'undefined' &&
+			wp.autosave &&
+			wp.autosave.server
+		) {
+			wp.autosave.server.triggerSave();
+			return;
+		}
+
+		resetButton();
+		if ( window.console && console.warn ) {
 			console.warn( 'Unable to save post.' );
 		}
 	},
 
-	/**
-	 * Attempt to save the current post using the best available mechanism.
-	 *
-	 * @return {boolean} True if a save was dispatched, false if no save mechanism was found.
-	 */
-	attemptToSave: function() {
-		if ( typeof wp === 'undefined' ) {
-			return false;
+	shouldUseBlockEditorSave: function( thisDialog ) {
+		return (
+			typeof wp !== 'undefined' &&
+			wp.data &&
+			wp.data.select &&
+			wp.data.dispatch &&
+			wp.data.select( 'core/editor' ) &&
+			thisDialog &&
+			thisDialog.builder &&
+			thisDialog.builder.config &&
+			thisDialog.builder.config.editorType === 'standalone'
+		);
+	},
+
+	saveBlockEditor: function( thisDialog, resetButton ) {
+		var self = this;
+		var pendingContentChange = thisDialog &&
+			thisDialog.builder &&
+			thisDialog.builder.pendingContentChange;
+
+		Promise.resolve( pendingContentChange )
+			.catch( function() {} )
+			.then( function() {
+				return self.waitForPostSavingUnlock();
+			} )
+			.then( function() {
+				return self.dispatchBlockEditorSave();
+			} )
+			.then( resetButton )
+			.catch( function() {
+				resetButton();
+				if ( window.console && console.warn ) {
+					console.warn( 'Unable to save post.' );
+				}
+			} );
+	},
+
+	dispatchBlockEditorSave: function() {
+		var self = this;
+		var editorSelect = wp.data.select( 'core/editor' );
+		var editorDispatch = wp.data.dispatch( 'core/editor' );
+
+		if ( editorSelect.isSavingPost() ) {
+			return this.waitForPostSaveCompletion( false )
+				.then( function() {
+					return self.dispatchBlockEditorSave();
+				} );
 		}
 
-		// Block Editor.
-		if ( wp.data && wp.data.select( 'core/editor' ) ) {
-			if ( ! wp.data.select( 'core/editor' ).isSavingPost() ) {
-				wp.data.dispatch( 'core/editor' ).savePost();
-			}
-			return true;
+		if (
+			editorSelect.isEditedPostDirty &&
+			! editorSelect.isEditedPostDirty()
+		) {
+			return Promise.resolve();
 		}
 
-		// Classic Editor, and older versions of the Block Editor.
-		if ( wp.autosave && wp.autosave.server ) {
-			wp.autosave.server.triggerSave();
-			return true;
+		editorDispatch.savePost();
+		return this.waitForPostSaveCompletion( true );
+	},
+
+	waitForPostSavingUnlock: function() {
+		var editorSelect = wp.data.select( 'core/editor' );
+		if (
+			! editorSelect.isPostSavingLocked ||
+			! editorSelect.isPostSavingLocked()
+		) {
+			return Promise.resolve();
 		}
 
-		return false;
-	}
+		return new Promise( function( resolve ) {
+			var unsubscribe = wp.data.subscribe( function() {
+				editorSelect = wp.data.select( 'core/editor' );
+				if (
+					! editorSelect.isPostSavingLocked ||
+					! editorSelect.isPostSavingLocked()
+				) {
+					unsubscribe();
+					setTimeout( resolve, 0 );
+				}
+			} );
+		} );
+	},
+
+	waitForPostSaveCompletion: function( requireStart ) {
+		var editorSelect = wp.data.select( 'core/editor' );
+		var isSaving = editorSelect.isSavingPost() ||
+			( editorSelect.isAutosavingPost && editorSelect.isAutosavingPost() );
+
+		if ( ! requireStart && ! isSaving ) {
+			return Promise.resolve();
+		}
+
+		return new Promise( function( resolve ) {
+			var saveStarted = isSaving;
+			var checks = 0;
+			var unsubscribe = wp.data.subscribe( function() {
+				editorSelect = wp.data.select( 'core/editor' );
+				isSaving = editorSelect.isSavingPost() ||
+					( editorSelect.isAutosavingPost && editorSelect.isAutosavingPost() );
+
+				if ( isSaving ) {
+					saveStarted = true;
+				}
+
+				if ( saveStarted && ! isSaving ) {
+					unsubscribe();
+					setTimeout( resolve, 0 );
+					return;
+				}
+
+				checks++;
+				if ( checks >= 120 ) {
+					unsubscribe();
+					resolve();
+				}
+			} );
+		} );
+	},
 
 }
